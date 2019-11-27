15a98ba0b974a865a592a7f936e1b6c87982cc32
minhazf456 <minhazf@gmail.com>
2019-11-26 17:36:05 -0800

    Java Fun done

---
+++ java_fun/HelloWorld.java
@@ -0,0 +1,13 @@
+/// Class name has to match with the file name
+public class HelloWorld {
+    public static void main(String[] args) { ///  entry point for interpretor to execute the code. This is where our interpretor will look at when executing the code..
+        System.out.println("Hello World");
+    }
+}
+// In your terminal, navigate to the javaFun directory and compile our HelloWorld.java file.If you don't see any errors, that means the javac compiler successfully compiled your Java code!
+
+// In your terminal, navigate to the javaFun directory and compile our HelloWorld.java file.If you don't see any errors, that means the javac compiler successfully compiled your Java code!
+
+// javac HelloWorld.java
+
+// Upon success, the Java compiler automatically generates a .class file. This file contains bytecode.
